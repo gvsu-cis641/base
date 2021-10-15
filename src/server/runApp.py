c3361c312e2792b789c7f2498ac968553019739c
hyeonggyujang <38144438+hyeonggyujang@users.noreply.github.com>
2021-10-15 15:48:18 -0400

    Deployment config set

--- src/server/runApp.py
+++ src/server/runApp.py
@@ -2,5 +2,6 @@ from app import app
 from config import Config
 
 
+
 if __name__ == '__main__':
-    app.run(Config.IP_ADDRESS, Config.SERVER_PORT, debug=Config.DEBUG_MODE)
+    app.run(Config.SERVER_IP_ADDRESS, Config.SERVER_PORT, debug=Config.DEBUG_MODE)
