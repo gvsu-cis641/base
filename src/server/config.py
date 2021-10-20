7af4c287440606b43b429d923bb2579da7980c8c
hyeonggyujang <38144438+hyeonggyujang@users.noreply.github.com>
2021-10-20 01:55:09 -0400

    sign_up and sign_in api
    Both sign_in and sign_up back-end logic is now available

--- src/server/config.py
+++ src/server/config.py
@@ -8,3 +8,7 @@ class Config(object):
     SERVER_IP_ADDRESS = os.getenv('IP_ADDRESS')
     SERVER_PORT = os.getenv('PORT_NUMBER')
     DEBUG_MODE = os.getenv('DEBUG')
+
+    AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+    AWS_REGION_NAME = os.getenv('AWS_REGION_NAME')
