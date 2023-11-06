8ddd49c6a86fe28e0f504ed428514cf7edfad7db
Zach Veenstra <zach.veen.gr@gmail.com>
2023-11-06 16:22:10 -0500

    Added AuthenticationView stub file.

---
+++ src/SmartJournal/SmartJournal/Features/Authentication/AuthenticationView.swift
@@ -0,0 +1,18 @@
+//
+//  AuthenticationView.swift
+//  SmartJournal
+//
+//  Created by Zach Veenstra on 11/6/23.
+//
+
+import SwiftUI
+
+struct AuthenticationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AuthenticationView()
+}
