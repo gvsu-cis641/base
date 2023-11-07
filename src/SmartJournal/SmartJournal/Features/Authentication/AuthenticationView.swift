f9dc56740046892027bf8d6c6314ec60403fcda0
katzmann835 <katzmann@umich.edu>
2023-11-06 20:12:52 -0500

    Created the code structure, how to test code?

--- src/SmartJournal/SmartJournal/Features/Authentication/AuthenticationView.swift
+++ src/SmartJournal/SmartJournal/Features/Authentication/AuthenticationView.swift
@@ -9,7 +9,49 @@ import SwiftUI
 
 struct AuthenticationView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        Text("Journal Log In")
+        
+        Text("Enter Name: ")
+            .multilineTextAlignment(.leading)
+        Text("Username: ")
+            .padding()
+            .background(
+                Color.gray
+                    .brightness(0.4)
+            )
+            .foregroundStyle(Color.black)
+            .multilineTextAlignment(.leading)
+             
+        Text("Enter Password: ")
+            .multilineTextAlignment(.leading)
+        Text("Password: ")
+            .padding()
+            .background(
+                Color.gray
+                    .brightness(0.4)
+            )
+            .foregroundStyle(Color.black)
+            .multilineTextAlignment(.leading)
+        
+        Text("Login:")
+            .multilineTextAlignment(.leading)
+        Text("Login")
+            .padding()
+            .background(Color(red:0.0, green:71.0, blue:171.0))
+            .foregroundStyle(Color.white)
+            .multilineTextAlignment(.center)
+        
+        Text("Forgot Username/password?")
+        
+        Text("Click Here")
+            .foregroundStyle(Color.blue)
+        
+        Text("Create Account")
+            .padding()
+            .background(Color(red:0.0, green:71.0, blue:171.0))
+            .foregroundStyle(Color.white)
+            .multilineTextAlignment(.center)
     }
 }
 
