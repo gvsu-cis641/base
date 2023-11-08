f14b564afec235151bd78d545b74bdd0a6e7dd28
Zach Veenstra <zach.veen.gr@gmail.com>
2023-11-07 21:32:43 -0500

    Example of how to use TextFields.

--- src/SmartJournal/SmartJournal/Features/Authentication/AuthenticationView.swift
+++ src/SmartJournal/SmartJournal/Features/Authentication/AuthenticationView.swift
@@ -8,60 +8,91 @@
 import SwiftUI
 
 struct AuthenticationView: View {
+    
+    @State var username: String = String()
+    @State var password: String = String()
+    
     var body: some View {
-        
-        Text("Journal Log In")
-            .font(.system(size:40))
-            .fontWeight(.semibold)
-        
-        Text("Enter Name: ")
-            .multilineTextAlignment(.leading)
-        Text("Username: ")
+        VStack (alignment: .leading) {
+            Text("Journal Log In")
+                .font(.system(size:40))
+                .fontWeight(.semibold)
+            
+            TextField("Username", text: $username)
+                .modifier(TextFieldModifier())
+            
+            SecureField("Password", text: $password)
+                .modifier(TextFieldModifier())
+        }
+        .padding()
+    }
+//    var body: some View {
+//        
+//        Text("Journal Log In")
+//            .font(.system(size:40))
+//            .fontWeight(.semibold)
+//        
+//        Text("Enter Name: ")
+//            .multilineTextAlignment(.leading)
+//        Text("Username: ")
+//            .padding()
+//            .background(
+//                Color.gray
+//                    .brightness(0.4)
+//            )
+//            .foregroundStyle(Color.black)
+//            .multilineTextAlignment(.leading)
+//            .frame(width: 426, height: 79)
+//             
+//        Text("Enter Password: ")
+//            .multilineTextAlignment(.leading)
+//        Text("Password: ")
+//            .padding()
+//            .background(
+//                Color.gray
+//                    .brightness(0.4)
+//            )
+//            .foregroundStyle(Color.black)
+//            .multilineTextAlignment(.leading)
+//            .frame(width: 426, height: 79)
+//        
+//        Text("Login:")
+//            .multilineTextAlignment(.leading)
+//        Text("Login")
+//            .padding()
+//            .background(
+//                Color(red:0.0, green:71.0, blue:171.0)
+//                    .brightness(-0.5))
+//            .foregroundStyle(Color.white)
+//            .multilineTextAlignment(.center)
+//            .frame(width: 355, height: 129)
+//        
+//        Text("Forgot Username/password?")
+//        
+//        Text("Click Here")
+//            .foregroundStyle(Color.blue)
+//        
+//        Text("Create Account")
+//            .padding()
+//            .background(
+//                Color(red:0.0, green:71.0, blue:171.0)
+//                    .brightness(-0.5))
+//            .foregroundStyle(Color.white)
+//            .multilineTextAlignment(.center)
+//            .frame(width: 355, height: 96)
+//    }
+}
+
+struct TextFieldModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
             .padding()
             .background(
                 Color.gray
-                    .brightness(0.4)
+                .brightness(0.4)
             )
-            .foregroundStyle(Color.black)
-            .multilineTextAlignment(.leading)
-            .frame(width: 426, height: 79)
-             
-        Text("Enter Password: ")
-            .multilineTextAlignment(.leading)
-        Text("Password: ")
-            .padding()
-            .background(
-                Color.gray
-                    .brightness(0.4)
-            )
-            .foregroundStyle(Color.black)
-            .multilineTextAlignment(.leading)
-            .frame(width: 426, height: 79)
-        
-        Text("Login:")
-            .multilineTextAlignment(.leading)
-        Text("Login")
-            .padding()
-            .background(
-                Color(red:0.0, green:71.0, blue:171.0)
-                    .brightness(-0.5))
-            .foregroundStyle(Color.white)
-            .multilineTextAlignment(.center)
-            .frame(width: 355, height: 129)
-        
-        Text("Forgot Username/password?")
-        
-        Text("Click Here")
-            .foregroundStyle(Color.blue)
-        
-        Text("Create Account")
-            .padding()
-            .background(
-                Color(red:0.0, green:71.0, blue:171.0)
-                    .brightness(-0.5))
-            .foregroundStyle(Color.white)
-            .multilineTextAlignment(.center)
-            .frame(width: 355, height: 96)
+            .foregroundColor(.black)
+            .cornerRadius(10)
     }
 }
 
