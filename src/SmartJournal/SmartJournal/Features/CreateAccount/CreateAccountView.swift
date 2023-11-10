a19ab487c5df06cceb78a31bac0e08cc5e9a17a3
Zach Veenstra <zach.veen.gr@gmail.com>
2023-11-10 13:54:26 -0500

    Created an AuthenticationState that determines if we show AuthenticationView or not.

---
+++ src/SmartJournal/SmartJournal/Features/CreateAccount/CreateAccountView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateAccountView.swift
+//  SmartJournal
+//
+//  Created by Zach Veenstra on 11/10/23.
+//
+
+import SwiftUI
+
+struct CreateAccountView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateAccountView()
+}
---
+++ src/SmartJournal/SmartJournal/Logic/UserAuthenticationState.swift
@@ -0,0 +1,8 @@
+//
+//  UserAuthenticationState.swift
+//  SmartJournal
+//
+//  Created by Zach Veenstra on 11/10/23.
+//
+
+import Foundation
