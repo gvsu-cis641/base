2d043bfa51a4fa10422d5edbf96b370c23420d08
Zach Veenstra <zach.veen.gr@gmail.com>
2023-11-17 14:03:21 -0500

    Fixed CreateAccountView.

--- src/SmartJournal/SmartJournal/Features/CreateAccount/CreateAccountView.swift
+++ src/SmartJournal/SmartJournal/Features/CreateAccount/CreateAccountView.swift
@@ -6,57 +6,40 @@
 //
 
 import SwiftUI
-import Foundation
 
 struct CreateAccountView: View {
-    
+
     @State private var email = ""
     @State private var username = ""
     @State private var password = ""
     @State private var con_password = ""
-    
+
     private let STACK_SPACING: CGFloat = 50
-    
+
     var body: some View {
         NavigationStack {
             VStack (spacing: STACK_SPACING) {
-                VStack{
-                    Text("Create Account")
-                        .font(.title)
-                        .fontWeight(.semibold)
-                }
-                
+                Text("Create Account")
+                    .font(.title)
+                    .fontWeight(.semibold)
+
                 VStack {
                     TextField("Email", text: $email)
                         .modifier(InputField())
-                    
+
                     TextField("Username", text: $username)
                         .modifier(InputField())
-                    
+
                     SecureField("Password", text: $password)
                         .modifier(InputField())
-                    
+
                     SecureField("Confirm Password", text: $con_password)
                         .modifier(InputField())
-                    
+
                     Button(action: {}) {
                         Text("Login")
                     }
-                    .ActionButton(ButtonStyle())
-                }
-                
-                VStack {
-                    HStack{
-                        Text("Click ")
-                        
-                        NavigationLink("Here") {
-                            .AuthenticationView(View)
-                                .foregroundStyle(Color.blue)
-                        }
-                        
-                        Text(" to return to the login page")
-                        
-                    }
+                    .buttonStyle(ActionButton())
                 }
                 .padding()
             }
