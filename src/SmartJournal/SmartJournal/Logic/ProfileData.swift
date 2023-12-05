c98815dd3423ad3cceedb043be9f79dc1fdae7c6
usmanq <qureshiu@mail.gvsu.edu>
2023-12-05 12:45:55 -0500

    create account with firebase and add user to firebase database. added package.resolved file to gitignore

--- src/SmartJournal/SmartJournal/Logic/ProfileData.swift
+++ src/SmartJournal/SmartJournal/Logic/ProfileData.swift
@@ -56,6 +56,36 @@ class FirebaseManager {
         }
     }
     
-    // Add other backend-related functions here
+    func createAccountAndAddUser(email: String, username: String, password: String, con_password: String, completion: @escaping (Result<UserProfile, Error>) -> Void) {
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let error = error {
+                    completion(.failure(error))
+                    print(error.localizedDescription)
+                } else if let authResult = authResult {
+                    // User account created successfully, now add a new entry in the "users" collection
+                    let user = UserProfile(displayName: username, photoURL: nil, bio: nil, email: email)
+                    self.addUserToFirestore(uid: authResult.user.uid, user: user, completion: completion)
+                }
+            }
+        }
+
+        private func addUserToFirestore(uid: String, user: UserProfile, completion: @escaping (Result<UserProfile, Error>) -> Void) {
+            let userRef = Firestore.firestore().collection("users").document(uid)
+
+            userRef.setData([
+                "displayName": user.displayName,
+                "photoURL": user.photoURL ?? NSNull(),
+                "bio": user.bio ?? NSNull(),
+                "email": user.email
+            ]) { error in
+                if let error = error {
+                    completion(.failure(error))
+                    print(error.localizedDescription)
+                } else {
+                    completion(.success(user))
+                    print("User added to Firestore successfully")
+                }
+            }
+        }
 }
 
